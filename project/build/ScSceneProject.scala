9ab1c78fdff01e80a55afbbab4f892ed4510306d
Herr Ritschwumm <ritschwumm@djini.de>
2011-05-22 20:45:32 +0200

    updated libraries

--- project/build/ScSceneProject.scala
+++ project/build/ScSceneProject.scala
@@ -2,8 +2,8 @@ import sbt._
       
 final class ScSceneProject(info:ProjectInfo) extends DefaultProject(info) {
 	// dependencies
-	val scutil 	= "de.djini"	%% "scutil"		% "0.0.1"	% "compile"
-	val	scgeom	= "de.djini"	%% "scgeom"		% "0.0.1"	% "compile"
+	val scutil 	= "de.djini"	%% "scutil"		% "0.0.3"	% "compile"
+	val	scgeom	= "de.djini"	%% "scgeom"		% "0.0.2"	% "compile"
 	
 	// issue compiler warnings
 	override def compileOptions	= super.compileOptions ++ Seq(Unchecked)
